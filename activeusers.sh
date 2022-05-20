10a3582a5c62e5da53127e7041ed613ebf61e392
Erik Woodhead <erik.woodhead@ulb.be>
2022-05-20 15:33:59 +0200

    fix indent

--- activeusers.sh
+++ activeusers.sh
@@ -5,8 +5,8 @@ USERS=$(ps -ef | tail -n +2 | awk '{print $1}' | sort | uniq)
 if [ "$1" = '-a' ] || [ "$1" = '--all' ]; then
     MIN_ID=0
 elif [ -n "$1" ]; then
-     echo "$0: unrecognised argument: $1." > /dev/stderr
-     exit 1
+    echo "$0: unrecognised argument: $1." > /dev/stderr
+    exit 1
 else
     MIN_ID=$(awk '/^UID_MIN/ { print $2 }' /etc/login.defs)
 fi
